30d18f7ffbce6757549ebab306efb81027ea5ee2
Viviana <viviana.herrera.mena@est.una.ac.cr>
2020-11-08 18:54:28 -0600

    ResponsablePago

---
+++ Infrastructure.DataAccess/Entities/ResposablePago.vb
@@ -0,0 +1,14 @@
+﻿Public Class ResposablePago
+    Public Property CedulaRespPago As Integer
+    Public Property Nombre As String
+    Public Property Apellido1 As String
+    Public Property Apellido2 As String
+    Public Property Direccion As String
+    Public Property Distrito As String
+    Public Property Canton As String
+    Public Property Provincia As String
+    Public Property Telefono As Integer
+    Public Property NumCuenta As String
+    Public Property AutorizadoRetirar As Char
+
+End Class
