59805c2aebecd70fffb61a7ed66e173ccf7afb53
Graciela Vargas Orias <vargasorias@gmail.com>
2020-11-09 00:17:39 -0600

    Plato

---
+++ Infrastructure.DataAccess/Abstracts/ICrudPlatoRepository.vb
@@ -0,0 +1,11 @@
+﻿
+Public Interface ICrudPlatoRepository(Of Entity As Class)
+    Function obtenerTodo() As IEnumerable(Of Entity)
+    Function obtenerPorValor(value As String) As IEnumerable(Of Entity)
+    Function obtenerPorValorMoney(value As Double) As IEnumerable(Of Entity)
+    Function obtenerPorValorInt(entity As Integer) As IEnumerable(Of Entity)
+    Function añadir(entity As Entity) As String
+    Function editar(entity As Entity) As String
+
+End Interface
+
---
+++ Infrastructure.DataAccess/Entities/Plato.vb
@@ -0,0 +1,6 @@
+﻿Public Class Plato
+
+    Public Property IdPlato As Integer
+    Public Property NombrePlato As String
+    Public Property Costo As String
+End Class
